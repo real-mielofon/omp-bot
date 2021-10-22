12477d98b9294fbe809f27556714f92611dccba7
Alexey Ponomarev <mielofon@sprut.ru>
2021-10-22 06:22:13 +0300

    insert return

--- internal/app/commands/raiting/theService/callback_list.go
+++ internal/app/commands/raiting/theService/callback_list.go
@@ -20,41 +20,27 @@ func (c *RatingTheServiceCommander) CallbackList(callback *tgbotapi.CallbackQuer
 	err := json.Unmarshal([]byte(callbackPath.CallbackData), &parsedData)
 	if err != nil {
 		log.Printf("error json.Unmarshal %s", parsedData)
-	} else {
+		return
+	}
 
-		ratings, err := c.service.List(parsedData.Offset, itemsOnList)
-		if err != nil {
-			c.sendError(fmt.Sprintf("error json.Unmarshal %s", parsedData), callback.Message.Chat.ID)
-			return
-		}
+	ratings, err := c.service.List(parsedData.Offset, itemsOnList)
+	if err != nil {
+		c.sendError(fmt.Sprintf("error json.Unmarshal %s", parsedData), callback.Message.Chat.ID)
+		return
+	}
 
-		outputMsgText := "Here all the raitings: \n\n"
-		for i, p := range ratings {
-			outputMsgText += fmt.Sprintf("%3d: %s\n", uint64(i)+parsedData.Offset, p.String())
-		}
+	outputMsgText := "Here all the raitings: \n\n"
+	for i, p := range ratings {
+		outputMsgText += fmt.Sprintf("%3d: %s\n", uint64(i)+parsedData.Offset, p.String())
+	}
 
-		msg := tgbotapi.NewMessage(callback.Message.Chat.ID, outputMsgText)
-		var buttons []tgbotapi.InlineKeyboardButton
-		if int(parsedData.Offset)-itemsOnList >= 0 {
-			if _, err := c.service.Describe(parsedData.Offset - itemsOnList); err == nil {
-				// Add Prev buttun
-				serializedData, _ := json.Marshal(CallbackListData{
-					Offset: parsedData.Offset - itemsOnList,
-				})
-				callbackPath := path.CallbackPath{
-					Domain:       "raiting",
-					Subdomain:    "service",
-					CallbackName: "list",
-					CallbackData: string(serializedData),
-				}
-
-				buttons = append(buttons, tgbotapi.NewInlineKeyboardButtonData("Prev page", callbackPath.String()))
-			}
-		}
-		if _, err := c.service.Describe(parsedData.Offset + itemsOnList); err == nil {
-			// Add Next buttun
+	msg := tgbotapi.NewMessage(callback.Message.Chat.ID, outputMsgText)
+	var buttons []tgbotapi.InlineKeyboardButton
+	if int(parsedData.Offset)-itemsOnList >= 0 {
+		if _, err := c.service.Describe(parsedData.Offset - itemsOnList); err == nil {
+			// Add Prev buttun
 			serializedData, _ := json.Marshal(CallbackListData{
-				Offset: parsedData.Offset + itemsOnList,
+				Offset: parsedData.Offset - itemsOnList,
 			})
 			callbackPath := path.CallbackPath{
 				Domain:       "raiting",
@@ -62,27 +48,41 @@ func (c *RatingTheServiceCommander) CallbackList(callback *tgbotapi.CallbackQuer
 				CallbackName: "list",
 				CallbackData: string(serializedData),
 			}
-			buttons = append(buttons, tgbotapi.NewInlineKeyboardButtonData("Next page", callbackPath.String()))
-		}
-		if len(buttons) > 0 {
-			msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
-				tgbotapi.NewInlineKeyboardRow(
-					buttons...,
-				),
-			)
-		}
 
-		_, err = c.bot.Send(msg)
-		if err != nil {
-			log.Println("error send message %s", err)
-			return
+			buttons = append(buttons, tgbotapi.NewInlineKeyboardButtonData("Prev page", callbackPath.String()))
 		}
-		_, err = c.bot.AnswerCallbackQuery(tgbotapi.CallbackConfig{
-			CallbackQueryID: callback.ID,
+	}
+	if _, err := c.service.Describe(parsedData.Offset + itemsOnList); err == nil {
+		// Add Next buttun
+		serializedData, _ := json.Marshal(CallbackListData{
+			Offset: parsedData.Offset + itemsOnList,
 		})
-		if err != nil {
-			log.Println("error AnswerCallbackQuery %s", err)
-			return
+		callbackPath := path.CallbackPath{
+			Domain:       "raiting",
+			Subdomain:    "service",
+			CallbackName: "list",
+			CallbackData: string(serializedData),
 		}
+		buttons = append(buttons, tgbotapi.NewInlineKeyboardButtonData("Next page", callbackPath.String()))
+	}
+	if len(buttons) > 0 {
+		msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+			tgbotapi.NewInlineKeyboardRow(
+				buttons...,
+			),
+		)
+	}
+
+	_, err = c.bot.Send(msg)
+	if err != nil {
+		log.Println("error send message %s", err)
+		return
+	}
+	_, err = c.bot.AnswerCallbackQuery(tgbotapi.CallbackConfig{
+		CallbackQueryID: callback.ID,
+	})
+	if err != nil {
+		log.Println("error AnswerCallbackQuery %s", err)
+		return
 	}
 }
