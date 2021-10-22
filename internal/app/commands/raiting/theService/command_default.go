309c26239bc8b79dc9da19d19e3629b807222309
Alexey Ponomarev <mielofon@sprut.ru>
2021-10-22 06:32:27 +0300

    опечатка log.Printf

--- internal/app/commands/raiting/theService/command_default.go
+++ internal/app/commands/raiting/theService/command_default.go
@@ -13,7 +13,7 @@ func (c *RatingTheServiceCommander) Default(inputMsg *tgbotapi.Message) {
 
 	_, err := c.bot.Send(msg)
 	if err != nil {
-		log.Println("error send message %s", err)
+		log.Printf("error send message %s", err)
 		return
 	}
 }
